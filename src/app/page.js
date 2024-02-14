acce1029db8ffd2e5f89fdea11a51d04bed79995
Anay Verma <anayverma2147@gmail.com>
2024-02-14 14:40:19 +0530

    initial draft

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,62 @@
+"use client"
 import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
-
+import '../app/styles/global.css';
+import React, { useEffect, useRef  } from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch(error => {
+        console.error('Autoplay was prevented:', error);
+      });
+    }
+  }, []);
+
   return (
     <main>
-      <LoginLink>Sign in</LoginLink>
-      <RegisterLink>Sign up</RegisterLink>
+      <section>
+      <video autoPlay loop muted playsInline ref={videoRef} className="background-clip">
+            <source src="/background-clip.mp4" type="video/mp4" />
+          </video>
+        <nav>
+          <div className="navbar">
+            <Link href="/"> 
+                    <img className="icon-tag" src="/icon.jpg" alt="fghj"/>
+            </Link>
+            <ul>
+              <li>
+              <Link href="/">
+                  home
+                </Link>
+              </li>
+              <li>
+              <Link href="/">
+                Traffic <br/> rules
+                </Link>
+              </li>
+              <li>
+                <RegisterLink>Sign up</RegisterLink>
+              </li>
+              <li>
+                <LoginLink>Sign in</LoginLink>
+              </li>
+              <li>
+              <Link href="/">
+                About <br/> us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+      </section>
+      
+      
     </main>
   );
 }
+
